e90b9734b4c3a954038d013d201cd36599f80687
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2017-06-15 20:35:04 -0400

    Java Practice

---
+++ src/HackerRank/Strings/IPAddress.java
@@ -0,0 +1,24 @@
+package HackerRank.Strings;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+/**
+ * Created by vaisakh on 15/06/17.
+ */
+class IPAddress{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+
+    String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
+}
