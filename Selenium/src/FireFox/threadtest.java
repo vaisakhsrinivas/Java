8c4689fe5cc649139b1cff4641c6d58e8db9c788
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-11-14 15:24:33 -0500

    thread test

---
+++ Selenium/src/FireFox/threadtest.java
@@ -0,0 +1,46 @@
+package FireFox;
+
+/**
+ * Created by vaisakh on 14/11/20.
+ */
+class sampledemo implements Runnable {
+
+    private Thread t;
+    private String threadName;
+
+    sampledemo (String threadName){
+
+        this.threadName = threadName;
+    }
+
+    public void run()
+    {
+        while (true)
+            System.out.print(threadName);
+    }
+
+    public void start ()
+    {
+        if(t == null)
+        {
+            t = new Thread(this, threadName);
+            t.start();
+        }
+    }
+
+}
+
+public class threadtest {
+
+    public static void main (String [] args) {
+
+        sampledemo a = new sampledemo("a");
+        sampledemo b = new sampledemo("b");
+
+        b.start();
+        a.start();
+
+    }
+
+
+}
