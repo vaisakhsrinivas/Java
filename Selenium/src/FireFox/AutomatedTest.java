d4d72416e67421ed4380b4fd07126afc7ab01f72
vaisakhsrinivas <vaisakhsrinivas.m@husky.neu.edu>
2020-11-17 11:10:02 -0500

    thread test

--- Selenium/src/FireFox/AutomatedTest.java
+++ Selenium/src/FireFox/AutomatedTest.java
@@ -18,17 +18,17 @@ public class AutomatedTest {
         driver.get("http://s3.amazonaws.com/co.tulip.cdn/memtracking/index.html");
         Thread.sleep(2000);
         String before_selection = driver.findElement(By.id("statNumCrashes")).getText();
-        System.out.println(before_selection);
+        System.out.println(before_selection); //check to see if value is being picked correctly
         driver.findElement(By.id("filterPageData")).click();
         Thread.sleep(2000);
         String after_selection = driver.findElement(By.id("statNumCrashes")).getText();
-        System.out.println(after_selection);
+        System.out.println(after_selection); //check to see if value is being picked correctly
         Assert.assertTrue(after_selection != before_selection);
         driver.findElement(By.id("filterPageAnalytics")).click();
         Thread.sleep(2000);
         String after_analytics = driver.findElement(By.id("statNumCrashes")).getText();
         Assert.assertTrue(after_selection != after_analytics);
-        System.out.println(after_analytics);
+        System.out.println(after_analytics); //check to see if value is being picked correctly
         driver.close();
     }
 }
